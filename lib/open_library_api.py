9c54fe1c2ff07c090c750bddf4ef23dd5ab43689
Newton Manyisa <manyisanewton26@gmail.com>
2025-03-27 22:50:33 +0300

    i have learned how to sent the requests

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -1,3 +1,30 @@
+
+
+
+# import requests
+# import json
+
+
+# class Search:
+
+#     def get_search_results(self):
+#         search_term = "the lord of the rings"
+
+#         search_term_formatted = search_term.replace(" ", "+")
+#         fields = ["title", "author_name"]
+#         # formats the list into a comma separated string
+#         # output: "title,author_name"
+#         fields_formatted = ",".join(fields)
+#         limit = 1
+
+#         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+
+#         response = requests.get(URL)
+#         return response.content
+
+# results = Search().get_search_results()
+# print(results)
+
 import requests
 import json
 
